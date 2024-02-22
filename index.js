85714397b380b21ded0f7acc09d1a312f3a8a742
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-22 22:57:57 +0530

    add msg send option

--- index.js
+++ index.js
@@ -12,7 +12,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Run when client connects
 io.on('connection', socket => {
-    console.log('New WS Connection...');
+    // Welcome current user
+    socket.emit('message', 'Welcome to ChatCord');
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
 })
 
 const PORT = 3000 || process.env.PORT;
