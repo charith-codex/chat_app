a7388432115539601dcfff9b6e8cba040af75de0
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-23 21:01:08 +0530

    set message display option

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,24 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatBord Bot';
+
 //Run when client connects
 io.on('connection', socket => {
     // Welcome current user
-    socket.emit('message', 'Welcome to ChatCord');
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatBord!'));
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', (botName, 'A user has joined the chat'));
 
     // Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', (botName, 'A user has left the chat'));
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
+        io.emit('message', ('USER', msg));
     });
 })
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,15 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
+// Message from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message submit
@@ -15,4 +21,19 @@ chatForm.addEventListener('submit', e => {
 
     // Emit message to server
     socket.emit('chatMessage', msg);
+    
+    // Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 });
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
