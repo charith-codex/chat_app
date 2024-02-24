86be9536bf21f2a1dd18dc997c5b5ddc7a8f2125
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-02-24 19:01:24 +0530

    fix send details not showing bug

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ io.on('connection', socket => {
     socket.emit('message', formatMessage(botName, 'Welcome to ChatBord!'));
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', (botName, 'A user has joined the chat'));
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
     // Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', (botName, 'A user has left the chat'));
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', ('USER', msg));
+        io.emit('message', formatMessage('USER', msg));
     });
 })
 
